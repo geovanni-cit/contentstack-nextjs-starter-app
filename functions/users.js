9eb42e04027ee1392fe2725ba0232bcf480b16ca
Conti, Geovanni [ITSBR Non-J&J] <gconti@its.jnj.com>
2025-04-03 15:53:33 -0300

    Add users function test

---
+++ functions/users.js
@@ -0,0 +1,8 @@
+export default function handler(request, response) {
+  const users = [
+    {name: "Jack", age: "25"},
+    {name: "Rick", age: "28"},
+    {name: "Jane", age: "34"},
+  ];
+  response.status(200).send(users);
+}
